829235b31e275e185224ec830592afd9890c5bde
mcasademont9 <83952276+mcasademont9@users.noreply.github.com>
2025-09-03 13:35:52 +0200

    MCV updates to develop localy

--- scripts/run-worker.sh
+++ scripts/run-worker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Install the plugin in editable mode
+pip install --no-deps --editable /app/nomad/plugins/nomad-forematics
+
 python -m celery -A nomad.processing worker -B -l info  -Q celery
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+# # Set the PYTHONPATH to prioritize the local plugin directory
+# export PYTHONPATH=/app/nomad/plugins:$PYTHONPATH
+
+# # Install the plugin in editable mode (optional step if needed)
+# pip install --no-deps --editable /app/nomad/plugins/nomad-forematics
+echo ">>> Starting run.sh"
+echo "Installing local nomad-forematics plugin..."
+pip uninstall -y nomad-forematics || true
+pip install --no-deps --editable /app/nomad/plugins/nomad-forematics
+pip show nomad-forematics
+
+# Run NOMAD as before
 python -m nomad.cli admin run app --with-gui --gunicorn --host 0.0.0.0 $@
